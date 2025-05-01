2a70a1e7018229509b98b46e5e0a9bc57faf5c07
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-01 19:28:05 +0500

    prettier code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,15 @@ import { View, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-   <View>
-      <View >
+    <View>
+      <View>
         <Text>Welcome to Expo Router!</Text>
       </View>
-      <View >
+      <View>
         <Text>1. Open the app in your editor</Text>
         <Text>2. Edit this file to change the screen</Text>
         <Text>3. Save to reload the app</Text>
       </View>
-   </View>
+    </View>
   );
 }
