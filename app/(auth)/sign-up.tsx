2acea3a380aa653fd43182193ba7fddd4fa5df46
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-06 19:51:55 +0500

    basic auth nav

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function SignUp() {
+  return (
+    <View>
+      <Text>This is signup page</Text>
+    </View>
+  )
+}
---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function onBoarding() {
+  return (
+    <View>
+      <Text>This is welcome page</Text>
+    </View>
+  )
+}
