f92a61067ea6ca66f8b92a6fd3c083ae752641b2
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-06 20:40:04 +0500

    constant fields

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,100 @@
-export const onBoardingData = [
+import arrowDown from "@/assets/icons/arrow-down.png";
+import arrowUp from "@/assets/icons/arrow-up.png";
+import backArrow from "@/assets/icons/back-arrow.png";
+import chat from "@/assets/icons/chat.png";
+import checkmark from "@/assets/icons/check.png";
+import close from "@/assets/icons/close.png";
+import dollar from "@/assets/icons/dollar.png";
+import email from "@/assets/icons/email.png";
+import eyecross from "@/assets/icons/eyecross.png";
+import google from "@/assets/icons/google.png";
+import home from "@/assets/icons/home.png";
+import list from "@/assets/icons/list.png";
+import lock from "@/assets/icons/lock.png";
+import map from "@/assets/icons/map.png";
+import marker from "@/assets/icons/marker.png";
+import out from "@/assets/icons/out.png";
+import person from "@/assets/icons/person.png";
+import pin from "@/assets/icons/pin.png";
+import point from "@/assets/icons/point.png";
+import profile from "@/assets/icons/profile.png";
+import search from "@/assets/icons/search.png";
+import selectedMarker from "@/assets/icons/selected-marker.png";
+import star from "@/assets/icons/star.png";
+import target from "@/assets/icons/target.png";
+import to from "@/assets/icons/to.png";
+import check from "@/assets/images/check.png";
+import getStarted from "@/assets/images/get-started.png";
+import message from "@/assets/images/message.png";
+import noResult from "@/assets/images/no-result.png";
+import firstImage from "@/assets/images/firstImage.png";
+import secondImage from "@/assets/images/secondImage.png";
+import thirdImage from "@/assets/images/thirdImage.png";
+import signUpCar from "@/assets/images/signup-car.png";
+
+export const images = {
+  firstImage,
+  secondImage,
+  thirdImage,
+  getStarted,
+  signUpCar,
+  check,
+  noResult,
+  message,
+};
+
+export const icons = {
+  arrowDown,
+  arrowUp,
+  backArrow,
+  chat,
+  checkmark,
+  close,
+  dollar,
+  email,
+  eyecross,
+  google,
+  home,
+  list,
+  lock,
+  map,
+  marker,
+  out,
+  person,
+  pin,
+  point,
+  profile,
+  search,
+  selectedMarker,
+  star,
+  target,
+  to,
+};
+
+export const onboarding = [
   {
     id: 1,
-    title: "The perfect ride is just one tap away",
-    description: "Book a ride in seconds and enjoy a comfortable journey.",
-    image: null,
+    title: "The perfect ride is just a tap away!",
+    description:
+      "Your journey begins with Ryde. Find your ideal ride effortlessly.",
+    image: images.firstImage,
+  },
+  {
+    id: 2,
+    title: "Best car in your hands with Ryde",
+    description:
+      "Discover the convenience of finding your perfect ride with Ryde",
+    image: images.secondImage,
+  },
+  {
+    id: 3,
+    title: "Your ride, your way. Let's go!",
+    description:
+      "Enter your destination, sit back, and let us take care of the rest.",
+    image: images.thirdImage,
   },
 ];
+
+export const data = {
+  onboarding,
+};
