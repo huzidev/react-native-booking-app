d2b166bfd7d0bf4261b07d959209d1c2c861124b
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-06 20:14:56 +0500

    skip button on-boardin

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,16 @@
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { router } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function onBoarding() {
   return (
-    <SafeAreaView className='flex h-full items-center justify-center bg-white'>
-      <Text>This is welcome page</Text>
+    <SafeAreaView className="flex h-full items-center justify-center">
+      <TouchableOpacity
+        onPress={() => router.replace("/(auth)/sign-up")}
+        className="w-full justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
