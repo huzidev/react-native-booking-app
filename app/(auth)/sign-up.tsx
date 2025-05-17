82442ed21a9f2d9c92ef3c29d8294d8165d60cc2
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-17 21:50:13 +0500

    signup repeatable components

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,17 @@
-import { View, Text, ScrollView } from 'react-native'
+import { images } from '@/constants';
+import { View, Text, ScrollView, Image } from 'react-native'
 
 export default function SignUp() {
   return (
-    <ScrollView className='flex-1 bg-black'>
-      <View className='flex-1 bg-white'>
-        update
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
       </View>
     </ScrollView>
-  )
+  );
 }
