e12e1ccfcdc690b79b7f6826744b3cb59f9eaa17
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-17 22:51:57 +0500

    signin ui

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,6 @@
+import { images } from '@/constants';
 import { useState } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from "react-native";
 
 export default function SignIn() {
   const [form, setForm] = useState({
@@ -8,8 +9,15 @@ export default function SignIn() {
   });
 
   return (
-    <View>
-      <Text>This is signin page</Text>
-    </View>
-  )
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
