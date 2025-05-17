944166e1b6c1826822d7c076540e4bcce4018dc2
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-17 23:09:00 +0500

    auth via google

---
+++ components/OAuth.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function OAuth() {
+  return (
+    <View>
+      <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
+        <View className="flex-1 h-[1px] bg-general-100" />
+        <Text className="text-lg">Or</Text>
+        <View className="flex-1 h-[1px] bg-general-100" />
+      </View>
+    </View>
+  );
+}
