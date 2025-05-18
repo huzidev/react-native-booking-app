7fdc5db0bef9b927480d9d4b88b2417773cf8ec7
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-05-18 19:42:13 +0500

    raw sql queries

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,12 +1,32 @@
-import { neon } from '@neondatabase/serverless';
+import { neon } from "@neondatabase/serverless";
 
 const sql = neon(`${process.env.DATABASE_URL}`);
 
 export async function POST(request: Request) {
-    try {
-        
-    } catch (err) {
-        console.error("Error creating user:", error);
-    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  try {
+    const { name, email, clerkId } = await request.json();
+
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
     }
+
+    const response = await sql`
+        INSERT INTO users (
+            name,
+            email,
+            clerkId
+        )
+        VALUES (
+            ${name},
+            ${email},
+            ${clerkId}
+        )
+    `;
+  } catch (err) {
+    console.error("Error creating user:", err);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
